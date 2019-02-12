965848295e70cc94409d622e935fce1c5aea15bb
GhoustEr <47031401+GhoustEr@users.noreply.github.com>
2019-02-12 06:13:33 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Pro In RolePlay`,"http://twitch.tv/Dream")
+client.user.setGame(`صّلي عـّلى النّبّي`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
